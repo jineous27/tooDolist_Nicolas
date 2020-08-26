a1db6f14fcca03fa99ac80f718e57058004b6f12
Jinhee Yi <jinhee.yi27@gmail.com>
2020-08-27 07:03:57 +1000

    Updated - Working on currentUser

--- clock.js
+++ clock.js
@@ -7,11 +7,12 @@ function getLiveTime () {
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
-}
+    clockTitle.innerText = `${hours < 10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}`: seconds}`;
+    }
 
 function init () {
     getLiveTime();
+    setInterval(getLiveTime, 1000);
 }
 
 init(); 
