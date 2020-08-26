4a6a9576f6612c198c178a526aac8dacd2d2f9e7
Jinhee Yi <jinhee.yi27@gmail.com>
2020-08-27 07:28:31 +1000

    The function to save your name on local storage

--- greetting.js
+++ greetting.js
@@ -6,18 +6,34 @@ const form = document.querySelector(".js-form"),
 const USER_LS = "currentUser",
     SHOWING_CN = "showing";
 
+function saveName(text) {
+    localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+    event.preventDefault(); //여기서는 설정된 기본값 이벤트를 막아버리는 기능. 엔터 눌러도 안됨//
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName() {
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit", handleSubmit) //이벤트가 발생하면 어디론가 가버린다//
+}
 
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
     greeting.innerText = `Hello  ${text}`;
+    //컴퓨터는 해석을 제일 위에서 부터 한다. 폼을 삭제하고, 인사를 추가하고, 제일 마지막에 내가 설정한 텍스를 넣는다//
 }
 
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-
-    } else {
+        askForName();
+    } else { 
         paintGreeting(currentUser);
     }
 }
