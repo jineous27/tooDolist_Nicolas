65f2041fd6d89e6b1769571ad42c29343e7b302b
Jinhee Yi <jinhee.yi27@gmail.com>
2020-08-27 06:35:56 +1000

    Revert "Add an interval function on the live time & '0' in front of 1 digit time (Making a JS Clock)"
    This reverts commit f9eecb2ad8b607a08327c4d89b29851529781c08.

--- clock.js
+++ clock.js
@@ -7,12 +7,11 @@ function getLiveTime () {
     const minutes = date.getMinutes();
     const hours = date.getHours();
     const seconds = date.getSeconds();
-    clockTitle.innerText = `${hours < 10 ? `0${hours}`: hours}:${minutes < 10 ? `0${minutes}` : minutes}:${seconds < 10 ? `0${seconds}`: seconds}`;
-    }
+    clockTitle.innerText = `${hours}:${minutes}:${seconds}`;
+}
 
 function init () {
     getLiveTime();
-    setInterval(getLiveTime, 1000);
 }
 
 init(); 
